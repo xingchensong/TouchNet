88a3d862b8220d2e4d4fa6e5ca77e1bee5494e27
xingchensong <xingchensong1996@163.com>
2025-03-10 04:51:41 +0000

    [train] 2nd commit, pipeline parallel enable loss print

--- examples/debug/download_c4.py
+++ examples/debug/download_c4.py
@@ -1,4 +1,9 @@
-from datasets import load_dataset
+from datasets import DownloadConfig, load_dataset
+
+download_config = DownloadConfig(
+    num_proc=12,
+    max_retries=1200,
+)
 
 # English only
-en = load_dataset("allenai/c4", "en")
+en = load_dataset("allenai/c4", "en", download_config=download_config)
