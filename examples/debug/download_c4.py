9058070423248773ddb499b1a259b54a755b1285
xingchensong <xingchensong1996@163.com>
2025-03-23 08:16:24 +0000

    [exp]: Download fineweb-edu, add note for number of tokens

--- examples/debug/download_c4.py
+++ examples/debug/download_c4.py
@@ -6,4 +6,14 @@ download_config = DownloadConfig(
 )
 
 # English only
-en = load_dataset("allenai/c4", "en", download_config=download_config)
+signal = 1
+while signal:
+    try:
+        # 305GB, 156B tokens
+        # ref: https://mp.weixin.qq.com/s?__biz=MjM5ODExNDA2MA==&mid=2449950449&idx=1&sn=dcafccb19ef913e905a5b6479a570fe3&chksm=b13c4092864bc9846be7a8bcb2f90d83e40ec5bd6c7e45c8c01f97d884f69213f93c586c9d6c#rd  # noqa
+        en = load_dataset("allenai/c4", "en", download_config=download_config)
+        # multilingual (mC4): 9.7TB (108 subsets, one per language), ~6T tokens
+        # en = load_dataset("allenai/c4", "multilingual", download_config=download_config)
+        signal = 0
+    except Exception as ex:
+        pass
--- examples/debug/download_wikitext.py
+++ examples/debug/download_wikitext.py
@@ -1,6 +1,8 @@
 from datasets import load_dataset
 
 prefix = "/bucket/output/jfs-hdfs/user/xingchen.song/share/huggingface/datasets/converted_jsonl_xcsong"
+
+# 548.05 MB, 103M tokens, 0.103B tokens
 train_data = load_dataset('Salesforce/wikitext', 'wikitext-103-v1', trust_remote_code=True)
 for k in train_data.keys():
     print(f"len of wikitext-103-v1[{k}]: {len(train_data[k])}")
@@ -9,6 +11,8 @@ for k in train_data.keys():
         batch_size=64,
         num_proc=32,
     )
+
+# 13.34 MB, 2M tokens, 0.002B tokens
 train_data = load_dataset('Salesforce/wikitext', 'wikitext-2-v1', trust_remote_code=True)
 for k in train_data.keys():
     print(f"len of wikitext-2-v1[{k}]: {len(train_data[k])}")
